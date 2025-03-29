6ab9204d2a32bfb18c24f396ad56ca99bb60a90b
Stefan <Stefan@DESKTOP-54LA564>
2025-03-29 13:45:43 +0100

    Add project files.

---
+++ test 1/test_1.py
@@ -0,0 +1,34 @@
+﻿import random
+
+def igra():
+    print("Dobrodošao u igru pogađanja broja!")
+    print("Pogađaj broj između 1 i 100.")
+    
+    # Računanje slučajnog broja između 1 i 100
+    tacan_broj = random.randint(1, 100)
+    
+    pokusaji = 0
+    pogodjen = False
+    
+    # Petlja koja traje dok igrač ne pogodi broj
+    while not pogodjen:
+        try:
+            # Unos broja od strane korisnika
+            korisnicki_broj = int(input("Unesi broj: "))
+            pokusaji += 1
+            
+            # Proverava da li je korisnik pogodio broj
+            if korisnicki_broj < tacan_broj:
+                print("Broj je veći. Pokušaj ponovo.")
+            elif korisnicki_broj > tacan_broj:
+                print("Broj je manji. Pokušaj ponovo.")
+            else:
+                pogodjen = True
+                print(f"Čestitam! Pogodio si broj {tacan_broj} u {pokusaji} pokušaja.")
+        
+        except ValueError:
+            print("Molim te unesi važeći broj.")
+            
+if __name__ == "__main__":
+    igra()
+
